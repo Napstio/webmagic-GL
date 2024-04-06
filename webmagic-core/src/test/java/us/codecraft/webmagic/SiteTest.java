6da0a087591b145dae3d6bf35b0557a9c58ae984
Napstio <NapstioTheRobot@outlook.fr>
2024-04-06 16:11:55 +0200

    changed the name of a function to be more understandable

--- webmagic-core/src/test/java/us/codecraft/webmagic/SiteTest.java
+++ webmagic-core/src/test/java/us/codecraft/webmagic/SiteTest.java
@@ -8,8 +8,9 @@ import org.junit.Test;
 
 public class SiteTest {
 
+    //this test function seems to set the default charset of the site and test if it was set correctly
     @Test
-    public void test() {
+    public void defaultCharsetCorrectlySetTest() {
         Site site = Site.me().setDefaultCharset(StandardCharsets.UTF_8.name());
         assertEquals(StandardCharsets.UTF_8.name(), site.getDefaultCharset());
     }
